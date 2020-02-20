ceda4470281075a812eeb1a67e66f4b64be62e19
Williamsrm9 <59450071+Williamsrm9@users.noreply.github.com>
2020-02-19 19:57:03 -0500

    Update Bad Grade Insurance Calculator.cpp

--- Bad Grade Insurance Calculator.cpp
+++ Bad Grade Insurance Calculator.cpp
@@ -8,6 +8,7 @@ int main()
 int courseQuantity;
 int q;
 int courseCost[10];
+int studentYear;
 
 cout << "Please enter the number of courses that you would like to buy insurance for this semester:" << endl;
 cin >> courseQuantity;
@@ -15,9 +16,27 @@ cin >> courseQuantity;
 for(q = 1; q <= courseQuantity; q++){
     
     cout << "Enter the price for class #" << q << ": " << endl;
+    cout << "$";
     cin >> courseCost[(q-1)];
-    
-    
+}
+
+cout << "What year of college are you in?" << endl;
+cout << "If you are a freshman, press 1" << endl;
+cout << "If you are a sophomore, press 2" << endl;
+cout << "If you are a junior, press 3" << endl;
+cout << "If you are a senior, press 4" << endl;
+cout << "If you have been in college for more than four years, press 5" << endl;
+
+cin >> studentYear;
+
+switch(studentYear){
+
+      case 1: cout << "You have selected that you are in your first year of college (Freshman)" << endl; break;
+      case 2: cout << "You have selected that you are in your second year of college (Sophomore)" << endl; break;
+      case 3: cout << "You have selected that you are in your third year of college (Junior)" << endl; break;
+      case 4: cout << "You have selected that you are in your fourth year of college (Senior)" << endl; break;
+      case 5: cout << "You have selected that you have been in college for more than four years" << endl; break;
+      default: cout << "This is not a valid selection. Please select a suitable option or exit." << endl;
 }
     
     
