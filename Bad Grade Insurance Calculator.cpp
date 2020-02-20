26a133d681dc4c5b8a4143a7495168cd21b0f010
Williamsrm9 <59450071+Williamsrm9@users.noreply.github.com>
2020-02-19 19:25:27 -0500

    Create Bad Grade Insurance Calculator.cpp

---
+++ Bad Grade Insurance Calculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+int courseQuantity;
+int q = 0;
+
+cout << "Please enter the number of courses that you would like to buy insurance for this semester:" << endl;
+cin >> courseQuantity;
+
+for(q <= courseQuantity; q++) {
+    cout << "Enter the price for class #" << q << ": " << endl;
+    
+}
+
+cout << "For loop is now broken, proceed with code" << endl;
+
+}
