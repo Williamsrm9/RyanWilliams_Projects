3c2207c536de7ea1405637da43034e6dbefe2a81
Williamsrm9 <59450071+Williamsrm9@users.noreply.github.com>
2020-02-20 01:50:54 -0500

    Update Bad Grade Insurance Calculator.cpp

--- Bad Grade Insurance Calculator.cpp
+++ Bad Grade Insurance Calculator.cpp
@@ -10,8 +10,14 @@ int q;
 int courseCost[10];
 char courseName[10][150];
 int studentYear;
+double GPA;
 int gradeProtection;
 int courseSubtotal;
+double courseTotalSum;
+double courseDeduction;
+double yearDeduction;
+double gpaDeduction;
+double protectionDeduction;
 
 cout << "Please enter the number of courses that you would like to buy insurance for this semester:" << endl;
 cin >> courseQuantity;
@@ -25,7 +31,9 @@ for(q = 0; q < courseQuantity; q++){
     cout << "$";
     cin >> courseCost[q];
     
-    courseSubtotal += courseCost[q];
+    
+    courseTotalSum += courseCost[q];
+    courseSubtotal = (0.50 * courseTotalSum);
     
 
 }
@@ -49,6 +57,17 @@ switch(studentYear){
       default: cout << "This is not a valid selection. Please select a suitable option or exit." << endl;
 }
 
+if(studentYear > 1){
+    cout << "Enter your current your current GPA (round to two decimal places):" << endl;
+    cin >> GPA;
+}
+
+else{
+    
+    GPA = 3.40;
+}
+
+
 cout << "Which grades would you like to buy protection for?" << endl;
 cout << "If you would like to purchase protection for Fs, press 1" << endl;
 cout << "If you would like to purchase protection for Fs and Ds, press 2" << endl;
@@ -66,7 +85,89 @@ switch(gradeProtection){
       default: cout << "This is not a valid selection. Please select a suitable option or exit." << endl;
 }
     
-
+if(2 > courseQuantity > 5){
     
+    courseDeduction = courseSubtotal - (0.10 * courseSubtotal); 
+}
+
+else if(courseQuantity >= 5){
+    
+    courseDeduction = courseSubtotal - (0.15 * courseSubtotal);
+}
+
+else{
+    
+    courseDeduction = courseSubtotal;
+}
+
+if(studentYear < 3){
+    
+    yearDeduction = courseDeduction - (0.10 * courseDeduction);
+}
+
+else{
+    
+    yearDeduction = courseDeduction;
+}
+
+if(GPA >= 3.50){
+    
+    gpaDeduction = yearDeduction - (0.15 * yearDeduction);
+}
+
+else if(3.25 <= GPA < 3.50){
+    
+    gpaDeduction = yearDeduction - (0.05 * yearDeduction);
+}
+
+else if(3.00 <= GPA < 3.25){
+    
+    gpaDeduction = yearDeduction + (0.05 * yearDeduction);
+}
+    
+else if(2.75 <= GPA < 3.00){
+    
+    gpaDeduction = yearDeduction + (0.15 * yearDeduction);
+}
+
+else if(2.50 <= GPA < 2.75){
+    
+    gpaDeduction = yearDeduction + (0.20 * yearDeduction);
+}
+
+else{
+    
+    gpaDeduction = yearDeduction + (0.20 * yearDeduction);
+}
+
+if(gradeProtection = 1){
+    
+    protectionDeduction = gpaDeduction - (0.05 * gpaDeduction);
+}
+
+else if(gradeProtection = 2){
+    
+    protectionDeduction = gpaDeduction + (0.05 * gpaDeduction);
+}
+
+else if(gradeProtection = 3){
+    
+    protectionDeduction = gpaDeduction + (0.10 * gpaDeduction);
+}
+
+else{
+    
+    protectionDeduction = gpaDeduction + (0.20 * gpaDeduction);
+}
+
+cout << "REVIEW PURCHASE:" << endl;
+
+for(q = 0; q < courseQuantity; q++){
+    
+    cout << courseName[q] << ": $" << courseCost[q] << endl;
+}
+cout << "Total cost of classes you want to insure for this semester: $" << courseSubtotal << endl;
+cout << "" << endl;
+cout << "Insurance plan covering the following courses at these prices costs: $" << protectionDeduction << endl; 
 
 }
